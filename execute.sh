fd2c6abb734ba108f212e02cba0c26cdc2399421
Renato Araujo Rizzo <renato.rizzo@dcomp.sor.ufscar.br>
2019-06-08 12:58:15 -0300

    Update execute.sh
    Modificado nome aplicação

--- execute.sh
+++ execute.sh
@@ -1,2 +1,2 @@
-docker build -t docker-flask ./
-docker run -p 5000:5000 -it --name docker-flask docker-flask
+docker build -t docker-appclient ./
+docker run -p 5000:5000 -it --name docker-appclient docker-appclient
