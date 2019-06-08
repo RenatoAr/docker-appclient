cec9371b82c95325ce9531d10d4197f953b62f26
Renato Araujo Rizzo <renato.rizzo@dcomp.sor.ufscar.br>
2019-06-08 16:47:17 -0300

    Update app.py
    teste

--- src/app.py
+++ src/app.py
@@ -6,17 +6,14 @@ import docker
 app = Flask(__name__)
 
 dados = 'nada'
+myCmd = 'ls'
 # client = docker.from_env()
 # container = client.containers.get('docker-flask')
-client = docker.from_env()
-objdocker = docker.Client(base_url="unix://var/run/docker.sock")
-objdocker.stats_container = objdocker.stats('docker-appclient')
-for stat in objdocker.stats_container:
-print(stat)
 
 @app.route('/', methods=['GET'])
 def getinfo():
     global dados
+    os.system(myCmd)
     # global container
     #return 'Hello '+ container
     return 'Hello '+ dados
@@ -28,5 +25,5 @@ def postinfo():
     return 'hello' + dados
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
