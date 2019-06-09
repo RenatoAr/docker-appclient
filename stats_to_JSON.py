1cebd0246719f805bf10e19c3731897b6b6c86d9
Renato Araujo Rizzo <renato.rizzo@dcomp.sor.ufscar.br>
2019-06-08 23:00:10 -0300

    Create stats_to_JSON.py
    Python script para criar JSON com os dados do container

---
+++ stats_to_JSON.py
@@ -0,0 +1,19 @@
+import json
+import datetime
+import os
+
+def dockerStatsToJSON (path, filename, data):
+	filePathNameWExt = './' + path + './' + filename + '.json'
+	with open(filePathNameWExt, 'w') as fp:
+		json.dump(data, fp)
+
+path = './'
+filename = 'dockerStats'
+timestamp = datetime.datetime.now()
+data = {}
+#Data
+data['timestamp'] = str(timestamp)
+#ID da Imagem
+data['imageID'] = os.popen('sudo docker images --filter=reference=docker-appclient --format "table {{.ID}}\t"').read()
+#Dados do Container
+data['containerStats'] = os.popen('sudo docker stats --no-stream --format "table {{.Name}}\t{{.ID}}\t{{.CPUPerc}}\t{{.MemUsage}}"').read()
