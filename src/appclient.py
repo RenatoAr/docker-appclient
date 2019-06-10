2d1a2b17e7113a106b9fc81f64d90fcb62e17502
renatoar <renato.rizzo@dcomp.sor.ufscar.br>
2019-06-10 00:36:49 -0300

    Atualização

--- src/appclient.py
+++ src/appclient.py
@@ -1,23 +1,24 @@
 import os
-import statsToJSON
 
 from flask import Flask, request
-import docker
+
+cmd = 'docker ps -a'
 
 app = Flask(__name__)
 
 dados = 'Hello World\n'
 
-@app.route('/', methods=['GET'])
+@app.route('/GET_INFO', methods=['GET'])
 def getinfo():
     global dados
+    os.system(cmd)
     return dados
 
-@app.route('/', methods=['POST'])
+@app.route('/POST_INFO', methods=['POST'])
 def postinfo():
     global dados
     dados = request.form['nome']
     return dados
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug:True)
+    app.run(host='0.0.0.0', debug=True)
